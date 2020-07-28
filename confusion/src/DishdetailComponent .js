4278b4b0b741e9aae4b4ac1b1d53d7a9e174b164
Assyl99 <salaha@student.mini.pw.edu.pl>
2020-07-28 03:13:22 +0200

    React Animation Components

--- confusion/src/DishdetailComponent .js
+++ confusion/src/DishdetailComponent .js
@@ -4,6 +4,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
  import {Link} from 'react-router-dom';
  import {Loading} from './LoadingComponent';
  import { baseUrl } from './shared/baseUrl';
+ import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
  const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -14,17 +15,19 @@ function RenderComments({comments , postComment , dishId}) {
     }
     const comment = comments.map(comment => {
         return (
+            <Fade in >
             <li key={comment.id}>
-                <p>{comment.comment}</p>
-                <p>-- {comment.author},
-                {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat */}
-                {new Intl.DateTimeFormat('en-US', {
-                        year: 'numeric',
-                        month: 'short',
-                        day: '2-digit'
-                    }).format(new Date(comment.date))}
-                </p>
+               
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author},
+                    {new Intl.DateTimeFormat('en-US', {
+                            year: 'numeric',
+                            month: 'short',
+                            day: '2-digit'
+                        }).format(new Date(comment.date))}
+                    </p>
             </li>
+            </Fade>
            
         )
     })
@@ -32,8 +35,10 @@ function RenderComments({comments , postComment , dishId}) {
         <div className='col-12 col-md-5 m-1'>
             <h4> Comments </h4>
             <ul className='list-unstyled'>
+                <Stagger in >
                 {comment}
                 <CommentForm dishId={dishId} postComment={postComment}/>
+                </Stagger>
             </ul>
 
         </div>
@@ -43,6 +48,11 @@ function RenderComments({comments , postComment , dishId}) {
     if (dish != null) {
         return (
             <div className='col-12 col-md-5 m-1'>
+                <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
                 <Card>
                     <CardImg width="100%"  src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
@@ -50,6 +60,7 @@ function RenderComments({comments , postComment , dishId}) {
                         <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
+                </FadeTransform>
             </div>
         )
     }
