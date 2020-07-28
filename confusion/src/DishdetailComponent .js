60c7be91d8d6a322ffc0e0198b9538cd57487de8
Assyl99 <salaha@student.mini.pw.edu.pl>
2020-07-28 02:35:39 +0200

    Fetch from Server

--- confusion/src/DishdetailComponent .js
+++ confusion/src/DishdetailComponent .js
@@ -3,6 +3,8 @@ import { Card, CardImg, CardBody, CardText, CardTitle , BreadcrumbItem , Breadcr
 import { Control, LocalForm, Errors } from 'react-redux-form';
  import {Link} from 'react-router-dom';
  import {Loading} from './LoadingComponent';
+ import { baseUrl } from './shared/baseUrl';
+
  const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -42,7 +44,7 @@ function RenderComments({comments , addComment , dishId}) {
         return (
             <div className='col-12 col-md-5 m-1'>
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg width="100%"  src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
@@ -75,8 +77,7 @@ const DishDetail = (props) => {
             </div>
         );
     }
-    
-  else  if (props.dish != null) {
+      else  if (props.dish != null) {
         return (
             <div className="container">
             <div className="row">
